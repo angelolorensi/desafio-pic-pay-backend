47991e3a207162e70a7cdf89106a36e6431c6388
angelo lorensi <angelomozza@gmail.com>
2023-11-13 14:38:37 -0300

    creating user repository

---
+++ src/main/java/com/desafio/picpay/desafiopicpay/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.desafio.picpay.desafiopicpay.repository;
+
+import com.desafio.picpay.desafiopicpay.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByCpf(String cpf);
+
+    Optional<User> findUserById(Long id);
+
+}
