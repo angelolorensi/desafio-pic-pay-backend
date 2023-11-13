416792804df42c4ce1a624d659fe996613240a75
angelo lorensi <angelomozza@gmail.com>
2023-11-13 18:17:33 -0300

    creating GlobalExceptionHandler

--- src/main/java/com/desafio/picpay/desafiopicpay/controller/UserController.java
+++ src/main/java/com/desafio/picpay/desafiopicpay/controller/UserController.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> createUser(UserDTO userDTO){
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO){
         User newUser = userService.createUser(userDTO);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
---
+++ src/main/java/com/desafio/picpay/desafiopicpay/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.desafio.picpay.desafiopicpay.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return new ResponseEntity<>("Email or CPF already registered, please insert a new one!", HttpStatus.BAD_REQUEST);
+    }
+}
