af085a74d8aa892eccac8ce5475802d9b2b26ecd
angelo lorensi <angelomozza@gmail.com>
2023-11-13 15:54:41 -0300

    creating user dto

--- src/main/java/com/desafio/picpay/desafiopicpay/dto/UserDTO.java
+++ src/main/java/com/desafio/picpay/desafiopicpay/dto/UserDTO.java
@@ -1,6 +1,8 @@
 package com.desafio.picpay.desafiopicpay.dto;
 
+import com.desafio.picpay.desafiopicpay.model.UserType;
+
 import java.math.BigDecimal;
 
-public record UserDTO(String firstName, String lastName, String cpf, String email, String password) {
+public record UserDTO(String firstName, String lastName, String cpf, String email, String password, UserType userType, BigDecimal balance) {
 }
