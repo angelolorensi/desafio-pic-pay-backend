60e328a0e7cedfb755d0e62d56c0e6f9f608ee4c
angelo lorensi <angelomozza@gmail.com>
2023-11-13 14:58:21 -0300

    save user method in user service

--- src/main/java/com/desafio/picpay/desafiopicpay/service/UserService.java
+++ src/main/java/com/desafio/picpay/desafiopicpay/service/UserService.java
@@ -33,4 +33,8 @@ public class UserService {
         return userRepository.findUserById(id).orElseThrow(UserNotFoundException::new);
     }
 
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
 }
